04d1677e8b15b75f8a27f30fdf740be2ba16a177
HaltingColt522 <l.frotschi@web.de>
2025-12-21 17:57:40 +0100

    kernel: add basic typedefs

---
+++ kernel/include/types/limits.h
@@ -0,0 +1,6 @@
+/* kernel/core/types/limits.h contains the definitions of typelimits
+ *
+ * TODO: maybe add limits later, for now, types in ./types.h don't rely on 
+ * these limits here!
+ *
+ * */
