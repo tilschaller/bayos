b7dbc4e30e36497ad22add33057ce1fee86ff9c5
HaltingColt522 <l.frotschi@web.de>
2025-12-21 17:57:40 +0100

    kernel: add basic typedefs

---
+++ kernel/include/types/types.h
@@ -0,0 +1,27 @@
+/* kernel/core/types/types.h contains typedefs for datatype in thr form of
+ * [signed/unsigned][type][bits]_t
+ * 
+ */
+#ifndef _STDINT_H
+#define _STDINT_H
+
+typedef signed char             int8_t;
+typedef unsigned char           uint8_t;
+
+typedef signed short int        int16_t;
+typedef unsigned short int      uint16_t;
+
+typedef signed int              int32_t;
+typedef unsigned int            uint32_t;
+
+typedef signed long long int    int64_t;
+typedef unsigned long long int  uint64_t;
+
+/*Note: when subtracting two pointers on 64bit, the difference should not be 
+ * in range of int64_t*/
+typedef signed long long int    ptrdiff_t;
+
+/*I would say nothing should be bigger then 2^64 bytes ;)*/
+typedef unsigned long long int  size_t;
+
+#endif
