28f3d37b8c0ad13fd4266739e1d5dcb56ae1ddb0
HaltingColt522 <l.frotschi@web.de>
2025-12-22 15:41:51 +0100

    kernel: minor changes

--- kernel/src/stdio/printf.c
+++ kernel/src/stdio/printf.c
@@ -3,6 +3,10 @@
 
 int32_t printf(const char *restrict format, ...) {
         int32_t char_cnt = 0;
+
+        va_list ap;
+        va_start(ap, format);
+
         for(int i = 0; format[i] != '\0'; i++, char_cnt++) {
                 if(format[i] == '\\') {
                         i++;
@@ -30,6 +34,12 @@ int32_t printf(const char *restrict format, ...) {
                                 putchar(format[i]);
                                 char_cnt += 2;
                                 break;
+                        }
+                } else if(format[i] == '%') {
+                        i++;
+                        switch(format[i]) {
+                        case 'c':
+
                         }
                 } else {
                         putchar((uint8_t)format[i]);
