45db8aac35d27fc20b6c01b1ae50d797f0c2f191
HaltingColt522 <l.frotschi@web.de>
2025-12-22 15:50:46 +0100

    kernel: delete stuff from printf, works now

--- kernel/src/stdio/printf.c
+++ kernel/src/stdio/printf.c
@@ -4,8 +4,8 @@
 int32_t printf(const char *restrict format, ...) {
         int32_t char_cnt = 0;
 
-        va_list ap;
-        va_start(ap, format);
+        //va_list ap;
+        //va_start(ap, format);
 
         for(int i = 0; format[i] != '\0'; i++, char_cnt++) {
                 if(format[i] == '\\') {
@@ -35,12 +35,12 @@ int32_t printf(const char *restrict format, ...) {
                                 char_cnt += 2;
                                 break;
                         }
-                } else if(format[i] == '%') {
+                /*} else if(format[i] == '%') {
                         i++;
                         switch(format[i]) {
                         case 'c':
 
-                        }
+                        }*/
                 } else {
                         putchar((uint8_t)format[i]);
                         char_cnt++;
