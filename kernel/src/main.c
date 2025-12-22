ebd8457f9b920403a2e9fab8635901d8750be677
HaltingColt522 <l.frotschi@web.de>
2025-12-22 12:53:52 +0100

    kernel: solve merge conficts

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -11,14 +11,9 @@ void _start(uint64_t memory_map, uint64_t video_info) {
         video_mode_info *vinfo = phys_to_virt(video_info);
         fb_init(vinfo);
 
-<<<<<<< HEAD
         printf("#########################\n");
         printf("#    Welcome to Bayos   #\n");
         printf("#########################\n");
-
-=======
-        printf("bayos!\n");
         
->>>>>>> c5cd6ec (kernel: add basic \ -operators in printf())
         hcf();
 }
